0c53e87b2406b51828058a761f26f38232243094
Daler <dsarifkulo021@gmail.com>
2024-02-27 15:02:52 +0500

    fixed

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -28,6 +28,10 @@ export default function Document() {
 				<meta name="author" content="SOHIB ABDURAMON"></meta>
 				<meta property="og:title" content="Bosh sahifa"></meta>
 				<meta property="og:type" content="Learning management system"></meta>
+				<meta property="og:url" content="https://grafuz.uz/"></meta>
+				<meta property="og:image" content="https://yandex-images.clstorage.net/bcGS48224/114f49bs0/_MhHSixJCPxLmlz-4eRu5y7uOBvnOt3VkQtII-CoRh11xyLlub5NLmaaA-TyXZnJrCJUNwwNuxgTqrdf1YSKGPHFGlcdZASVMtdEg7HEYvMmm00Xo0u1xOViQVKE7HtkCc8LUVBzrPvbYtodLvR9taVfCXY5IK4xp177UwGEGtQkIA_6iAVJ7MOGvcqZuB7jPh84dy9HiXxBl41q8MmzQcTiB4GEnwSjj5Z2tXaIWZXx87HGhISa1cMT0_OVSDpEPLinkpw1cVRrajnaJbDCfyrLpIPfgwVADY_VZghFv4mYLuu9sJ_kR2sCgmHSAJ15ueuFhnykQhnPm6MzCagCDKyITwoQ2VGU59J12rHs7ucqm1QPs0v1VLXPkfKAbcfRoKbvtaR_eCfH0o5xisBdnYnboaYAoPJ5x0PL3-XojpR8DEeqcJ2tpPvKNaJR0PInMlfcgyPT1UTRd1WC5GkfccRS55VgQxj7_2q2kW4IceHJ81H2eMy2XQuLV_Od7NLAnDD_egg5FUw3xlVCZdxeT66jZBtD1_lcvSNh-ghlo3lA6m91tKes34MGTlmqWNmJjVvVegDwZtHza69rZSRO4JgU36K06YEc3zZZTpmkctO2l4wD50OhvP0DeW4UKR-F7IbrqZj7GCe3woKRakxl5UV7fXKAJFLFX_v__400wuzoIGeGNE0FiHcGsa7l5OLb0rM0Zyd7sWxd_x3KIHlD4Zg63w3w6zS7G_6CmYpoGQ29TxFKBJwq-XMPt9OtYBI4rFybKgg5TexHxoGKoSge31KzFLMzY_nUZWcx0qD1HxHAVmMN6Ofs_xtqXmkCfJkFEVO9uniUGtHTZz-L8RiqlPwAR34sHR2cazYdFpVk5tPyX6xH5yMh-H1HcbIcvUONGMondRBjiL8PRqp9ggB9BXGHOYZEPD5l7xOne4HYPphIpBMKYMUBQCNe1erJ_JIXModge2s_6axhq5k-iPV0"></meta>
+				<meta property="og:title" content="Bosh sahifa"></meta>
+				<meta property="og:type" content="website"></meta>
 			</Head>
 			<body>
 				<Main />
